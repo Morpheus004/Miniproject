6dc29f1eac0fc35edf736dd759cd0de622ddd9b3
Prasham Mehta <prashammehta04@gmail.com>
2024-04-12 18:27:29 +0530

    student page everything except apply button and profile page

---
+++ server/routes/alumniinternship.js
@@ -0,0 +1,43 @@
+import express from "express";
+import bcrypt from "bcrypt";
+import db from "../config/database.js";
+
+const router = express.Router();
+
+router.get("/api/alumni/internship", async (req, res) => {
+    try {
+      const { rows } = await db.query("SELECT * FROM internship");
+      console.log(rows);
+      res.json(rows);
+    } catch (error) {
+      console.error("Error fetching internships:", error);
+      res.status(500).json({ error: "An unexpected error occurred" });
+    }
+  });
+  
+router.post("/api/alumni/internship", async (req, res) => {
+    const { title,role,domain,applicants, date, location, description,duration } = req.body;
+    try {
+      const { rows } = await db.query(
+        "INSERT INTO internship (title,roles,domain_t,applicants, date, location, description,duration_months) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *;",
+        [title,role,domain,applicants, date, location, description,duration]
+      );
+      res.status(201).json(rows[0]);
+    } catch (error) {
+      console.error("Error adding internship:", error);
+      res.status(500).json({ error: "An unexpected error occurred" });
+    }
+  });
+  
+router.delete("/api/alumni/internship/:id", async (req, res) => {
+    const eventId = req.params.id;
+    try {
+      await db.query("DELETE FROM internship WHERE iid = $1", [eventId]);
+      res.sendStatus(204);
+    } catch (error) {
+      console.error("Error deleting internship:", error);
+      res.status(500).json({ error: "An unexpected error occurred" });
+    }
+  });
+
+  export default router;
--- server/routes/event.js
+++ server/routes/event.js
@@ -19,8 +19,8 @@ router.post("/api/events", async (req, res) => {
     const { title, date, location, description, seats } = req.body;
     try {
       const { rows } = await db.query(
-        "INSERT INTO events (title, date, description, seats) VALUES ($1, $2, $3, $4) RETURNING *;",
-        [title, date, description, seats]
+        "INSERT INTO events (title, date,location, description, seats,registeredStudents) VALUES ($1, $2, $3, $4,$5,0) RETURNING *;",
+        [title, date,location, description, seats]
       );
       res.status(201).json(rows[0]);
     } catch (error) {
@@ -39,5 +39,18 @@ router.delete("/api/events/:id", async (req, res) => {
       res.status(500).json({ error: "An unexpected error occurred" });
     }
   });
+  router.put("/api/events/:eventId/register", async (req, res) => {
+    const eventId = req.params.eventId;
+    try {
+      console.log("Received request to register for event with ID:", eventId);
+      const result = await db.query("UPDATE events SET registeredstudents = registeredstudents + 1 WHERE eid = $1", [eventId]);
+      console.log("Database update result:", result);
+    } catch (error) {
+      console.error("Error registering for event:", error);
+      res.status(500).send('Error registering for event');
+    }
+  });
+
+  
 
   export default router;
